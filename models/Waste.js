ca6d5479d1c322cd7059d4bf051a9b297cb2295b
“thegeralbeanQD” <giquigeraldine@outlook.com>
2023-05-09 10:30:38 +0100

    bug fix where if last_collection date is null the auto update breaks

--- models/Waste.js
+++ models/Waste.js
@@ -9,12 +9,12 @@ class Waste {
     }
 
     static async getAll() {
-        const response = await db.query(`SELECT COUNT(*) FROM waste`);
+        const response = await db.query(`SELECT COUNT(*) FROM wastes`);
         return parseInt(response.rows[0].count)
     }
 
     static async getId(postcode) {
-        const response = await db.query(`SELECT waste_id FROM waste WHERE waste_postcode = $1`, [postcode]);
+        const response = await db.query(`SELECT waste_id FROM wastes WHERE waste_postcode = $1`, [postcode]);
         if (response.rows.length != 1) {
             throw new Error("Unable postcode does not match any IDs.")
         }
@@ -23,7 +23,7 @@ class Waste {
 
     static async getOneById(id) {
         const response = await db.query(`SELECT W.waste_id, W.waste_postcode, R.recycling_days, R.recycling_last_collection, R.recycling_next_collection, G.general_days, G.general_last_collection, G.general_next_collection, C.compost_days, C.compost_last_collection, C.compost_next_collection 
-        FROM waste AS W LEFT JOIN recycling AS R ON W.waste_id = R.recycling_waste_id LEFT JOIN general AS G ON W.waste_id = G.general_waste_id LEFT JOIN compost AS C ON W.waste_id = C.compost_waste_id WHERE W.waste_id = $1`
+        FROM wastes AS W LEFT JOIN recycling AS R ON W.waste_id = R.recycling_waste_id LEFT JOIN general AS G ON W.waste_id = G.general_waste_id LEFT JOIN compost AS C ON W.waste_id = C.compost_waste_id WHERE W.waste_id = $1`
             , [id])
         if (response.rows.length != 1) {
             throw new Error("Unable to locate by postcode by ID.")
@@ -32,11 +32,11 @@ class Waste {
     }
 
     static async createNewPostcode(postcode) {
-        const exsists = await db.query('SELECT COUNT(*) FROM waste WHERE waste_postcode = $1', [postcode]);
+        const exsists = await db.query('SELECT COUNT(*) FROM wastes WHERE waste_postcode = $1', [postcode]);
         if (parseInt(exsists.rows[0].count) > 0) {
             throw new Error("Postcode exists")
         }
-        let NewPostcode = await db.query(`INSERT INTO waste (waste_postcode) VALUES ($1) RETURNING waste_id`,
+        let NewPostcode = await db.query(`INSERT INTO wastes (waste_postcode) VALUES ($1) RETURNING waste_id`,
             [postcode]);
         const newId = parseInt(NewPostcode.rows[0].waste_id);
         return newId;
@@ -44,7 +44,7 @@ class Waste {
 
     static async insertWasteData(wasteType, data, id) {
         const { days: wasteTypeDays, last_collection: wasteTypeLastCollection } = data;
-        const response = await db.query(`INSERT INTO ${wasteType} (${wasteType}_days, ${wasteType}_last_collection, ${wasteType}_waste_id) VALUES ($1, $2, $3)`,
+        await db.query(`INSERT INTO ${wasteType} (${wasteType}_days, ${wasteType}_last_collection, ${wasteType}_waste_id) VALUES ($1, $2, $3)`,
             [wasteTypeDays, wasteTypeLastCollection, id]);
         return await Waste.getOneById(id);
     }
@@ -60,6 +60,7 @@ class Waste {
     }
 
     static async create(data, id) {
+        console.log(data);
         await Waste.insertWasteData('recycling', { days: data.recycling_days, last_collection: data.recycling_last_collection }, id);
         await Waste.insertWasteData('general', { days: data.general_days, last_collection: data.general_last_collection }, id);
         await Waste.insertWasteData('compost', { days: data.compost_days, last_collection: data.compost_last_collection }, id);
@@ -96,36 +97,34 @@ class Waste {
     }
 
     static async destroyWasteData(wasteType, id) {
-        await db.query(`DELETE FROM ${wasteType} WHERE ${wasteType}_waste_id = $1;`, [id]);
+        const response = await db.query(`DELETE FROM ${wasteType} WHERE ${wasteType}_waste_id = $1 RETURNING *`, [id]);
+        if (response.rows.length != 1) {
+            throw new Error(`Unable to delete ${wasteType} data.`)
+        }
         return await Waste.getOneById(id)
     }
 
 
     static async destroy(id) {
-        let response = await Waste.destroyWasteData('recycling', id);
-        if (response.rows.length != 1) {
-            throw new Error("Unable to delete recycling data.")
-        }
-        response = await Waste.destroyWasteData('general', id);
-        if (response.rows.length != 1) {
-            throw new Error("Unable to delete general data.")
-        }
-        response = await Waste.destroyWasteData('compost', id);
-        if (response.rows.length != 1) {
-            throw new Error("Unable to delete compost data.")
-        }
-        response = await db.query('DELETE FROM waste WHERE waste_id = $1 RETURNING *;', [id]);
-        if (response.rows.length != 1) {
-            throw new Error("Unable to delete waste data with this ID.")
-        }
+        await Waste.destroyWasteData('recycling', id);
+        await Waste.destroyWasteData('general', id);
+        await Waste.destroyWasteData('compost', id);
+        await db.query('DELETE FROM waste WHERE waste_id = $1 RETURNING *;', [id]);
         return new Waste(response.rows[0]);
     }
 
-    static async autoUpdateData(data){
+
+    static async autoUpdateData(data) {
         const id = data.waste_id
-        await Waste.checkIfUpdated('recycling', { days: data.recycling_days, last_collection: data.recycling_last_collection }, id);
-        await Waste.checkIfUpdated('general', { days: data.general_days, last_collection: data.general_last_collection }, id);
-        await Waste.checkIfUpdated('compost', { days: data.compost_days, last_collection: data.compost_last_collection }, id);
+        if (data.recycling_last_collection != null){
+            await Waste.checkIfUpdated('recycling', { days: data.recycling_days, last_collection: data.recycling_last_collection }, id);
+        }
+        if (data.general_last_collection != null){
+            await Waste.checkIfUpdated('general', { days: data.general_days, last_collection: data.general_last_collection }, id);
+        }
+        if (data.compost_last_collection != null){
+            await Waste.checkIfUpdated('compost', { days: data.compost_days, last_collection: data.compost_last_collection }, id);
+        }
         return await Waste.getOneById(id);
     }
 
@@ -139,7 +138,7 @@ class Waste {
         updatedLastCollection = moment(updatedLastCollection).format('YYYY-MM-DD');
         let nextCollection = moment(updatedLastCollection).add(daysBetween, 'days')
         nextCollection = moment(nextCollection).format('YYYY-MM-DD')
-        
+
         if (moment(todayDate).isSame(nextCollection, 'day')) {
             nextCollection = moment(nextCollection).add(daysBetween, 'days')
             nextCollection = moment(nextCollection).format('YYYY-MM-DD')
@@ -157,14 +156,14 @@ class Waste {
         return Waste.getOneById(id)
     }
 
-    static async checkLastCollection(todayDate, lastCollection, daysBetween){
+    static async checkLastCollection(todayDate, lastCollection, daysBetween) {
         // console.log(todayDate, lastCollection, daysBetween);
         function addDays(date, days) {
             date.setDate(date.getDate() + days);
             return new Date(date);
         } //Function which will add days to the last collection
 
-        while (moment(todayDate).diff(moment(lastCollection), 'days') > daysBetween){
+        while (moment(todayDate).diff(moment(lastCollection), 'days') > daysBetween) {
             lastCollection = addDays(lastCollection, daysBetween)
         }
         return lastCollection
